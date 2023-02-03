1c1c96a5ffef9f77f7502871976bcbf2b1aecc51
Denis Seredin <sibwinter42@gmail.com>
2023-02-04 00:22:34 +0300

    flake8

--- api_yamdb/reviews/management/commands/import_csv_data.py
+++ api_yamdb/reviews/management/commands/import_csv_data.py
@@ -1,6 +1,5 @@
 from csv import DictReader
 from django.conf import settings
-
 from django.core.management import BaseCommand
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
