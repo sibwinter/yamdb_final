c755b9aac8387ee6ad0ccf5073a2fb194a2baa6f
Denis Seredin <sibwinter42@gmail.com>
2023-02-04 00:34:41 +0300

    flake8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,6 @@ from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
 from users.serializers import (RegisterSerializer, TokenSerializer,
