a7f19508d10131388640e64dd408af6c86478d18
Denis Seredin <sibwinter42@gmail.com>
2023-02-04 00:18:04 +0300

    flake8

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
 from reviews.validators import validate_year_to_current
-
 
 User = get_user_model()
 
